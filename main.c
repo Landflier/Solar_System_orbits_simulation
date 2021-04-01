1b5a63842a7954335b48acb86b3e0bf18276f97d
Landflier <ASdjkhafgq354jkq23gkfjasbdfjk>
2021-04-01 12:25:27 +0300

    raphs of hamiltonian and anular momentum

--- main.c
+++ main.c
@@ -22,15 +22,14 @@ void print_current_system(int N, Stellar_body System[N]){
 int main() {
     int N=10; // number of objects
     Stellar_body Objects[N];
-    long double total_mass=0;
 
     Read_Nasa_txt_file(N,Objects);
 
 
     long double k= 3; // k is in units of days
     int years= 5000;
-    //int Total_Number_of_steps = years * 365 / k;
-    int Total_Number_of_steps = 5000;
+    int Total_Number_of_steps = years * 365 / k;
+    //int Total_Number_of_steps = 5000;
 
 
 
@@ -44,21 +43,19 @@ int main() {
     reset_simulation_file(system_variables);
 
     FILE *simulation_file, *simulation_file_python, *system_variables_python;
-    simulation_file = fopen(human_readable_results, "a"); // progam is run from /bin
+    //simulation_file = fopen(human_readable_results, "a"); // progam is run from /bin
     simulation_file_python = fopen(python_results, "a");
     system_variables_python = fopen(system_variables, "a");
 
     Stellar_body  New_Object[N]; // updating solar system
     for (int i=0; i<N; i++){
         New_Object[i].mass = Objects[i].mass;
-        total_mass += Objects[i].mass;
     }
     printf("Starting system data: \n");
     print_current_system(N,Objects);
 
     //Write the value of k at the start of system_variables
-    fprintf(system_variables_python, " %Le \n ",
-            k);
+    fprintf(system_variables_python, " %Le \n ", k);
 
     for (int iteration_number = 0 ; iteration_number < Total_Number_of_steps ; iteration_number++ ){
 
@@ -72,15 +69,15 @@ int main() {
 
         Write_System_Variables(N,Objects, system_variables_python, total_mass);
 
-        Write_Solar_System_iteration(N,Objects, simulation_file);
-        fprintf(simulation_file, "\n"); // delimeter charecter between simulations for human readable
+        //Write_Solar_System_iteration(N,Objects, simulation_file);
+        //fprintf(simulation_file, "\n"); // delimeter charecter between simulations for human readable
 
         Write_Solar_System_iteration(N,Objects, simulation_file_python);
 
     }
 
     fclose(simulation_file_python);
-    fclose(simulation_file);
+    //fclose(simulation_file);
     fclose(system_variables_python);
 
 return 0;
--- python_visualization/Graphs_of_system_variables.py
+++ python_visualization/Graphs_of_system_variables.py
@@ -1,11 +1,53 @@
 import pandas as pd
 import numpy as np
-
+import datetime
+from datetime import date, timedelta
 
 import matplotlib.pyplot as plt
 
-
 system_variables = pd.read_csv ("../data/Simulation_Results/system_variables_python.txt", sep= ",",
-                                names= ["L_"])
+                                names= ["center_of_mass_x", "center_of_mass_y", "center_of_mass_z",
+                                        "angular_mom_x", "angular_mom_y", "angular_mom_z",
+                                        "hamiltonian"])
 
-print (system_variables)
+
+k = system_variables["center_of_mass_x"][0] #first row is k, i.e R-K step in days
+
+system_variables.drop([0], inplace=True)
+
+params = {'figure.figsize': (11.5, 8), # standard A4
+          'font.size': 12,
+          'figure.autolayout': True,
+          'legend.fontsize': 10,
+          #'legend.loc': 'upper center',
+          #'legend.bbox_to_anchor' : (10,0.5,0.5,0.5)
+          #'legend.handlelength': 4=(0.5, 0., 0.5, 0.5)
+         }
+#print(plt.rcParams.keys())  # prints every parameter key if need to edit anyhthing else
+plt.rcParams.update(params)
+
+all_dates = range(len(system_variables))
+
+fig = plt.figure()
+
+Hamiltonian_plot = fig.add_subplot(2,1,1)
+
+Hamiltonian_plot.plot (all_dates, system_variables["hamiltonian"] )
+Hamiltonian_plot.set_xlabel('Year')
+Hamiltonian_plot.set_ylabel('Energy (au^2*kg/day^-2')
+Hamiltonian_plot.set_title('Hamiltonian of solar system')
+
+Angular_plot = fig.add_subplot(2,1,2)
+
+angular_momentum_modulus = np.sqrt( system_variables["angular_mom_x"]**2 + system_variables["angular_mom_y"]**2 + system_variables["angular_mom_z"]**2 )
+
+print (angular_momentum_modulus)
+Angular_plot.plot (all_dates, angular_momentum_modulus)
+Angular_plot.set_xlabel('Year')
+Angular_plot.set_ylabel('Angular momentum modulus')
+Angular_plot.set_title('Angular momentum wrt barycenter of Solar System')
+
+
+
+
+plt.show()
